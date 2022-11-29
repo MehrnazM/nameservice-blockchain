789639e8f1c1ea77c8774160bf0a11892297b24c
MehrnazM <mehrnaz.movahed@gmail.com>
2022-11-28 19:44:57 -0800

    added keepers

--- x/nameservice/keeper/msg_server_set_name.go
+++ x/nameservice/keeper/msg_server_set_name.go
@@ -3,15 +3,33 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"nameservice/x/nameservice/types"
+
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) SetName(goCtx context.Context, msg *types.MsgSetName) (*types.MsgSetNameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	whois, found := k.GetWhois(ctx, msg.Name)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Name is not registered")
+	}
+
+	if whois.Owner != msg.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+	}
+
+	newWhois := types.Whois{
+		Name:  msg.Name,
+		Index: msg.Name,
+		Value: msg.Value,
+		Owner: whois.Owner,
+		Price: whois.Price,
+	}
+	k.SetWhois(ctx, newWhois)
 
 	return &types.MsgSetNameResponse{}, nil
 }
--- x/nameservice/types/errors.go
+++ x/nameservice/types/errors.go
@@ -8,5 +8,6 @@ import (
 
 // x/nameservice module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrSample            = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInsufficientFunds = sdkerrors.Register(ModuleName, 1200, "insufficient fund error")
 )
